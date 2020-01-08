c6c6a0e2d64d984e7a4416837386fac97e71affb
lewismunyi <lewismunyi97@gmail.com>
2020-01-08 11:40:51 +0300

    Initialize and authenticate functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,7 @@
-const functions = require('firebase-functions');
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require("firebase-functions");
+let serviceAccount = require("./firebase-adminsdk.json");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount)
+});
