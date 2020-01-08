845f931da3f079cd3fa64739c1863e28d2b124eb
lewismunyi <lewismunyi97@gmail.com>
2020-01-08 11:45:09 +0300

    Get single blog HTTP function

--- functions/index.js
+++ functions/index.js
@@ -13,3 +13,41 @@ admin.initializeApp({
 let db = admin.firestore();
 let FieldValue = admin.firestore.FieldValue;
 let FieldPath = admin.firestore.FieldPath;
+
+/*
+ * Create on post request function.
+ * This function is triggered when a user(bot) sends a http request with the document id/url.
+ * The function returns a blog post in JSON format
+ * */
+
+exports.getSingleBlogPostFromFirebase = functions.https.onRequest((req, res) => {
+	if (req.method !== "GET") {
+		return res.status(405).json({ message: "Method not allowed" });
+	}
+	function titleize(slug) {
+		var words = slug.split("-");
+		return words
+			.map(function(word) {
+				return word.charAt(0).toUpperCase() + word.substring(1).toLowerCase();
+			})
+			.join(" ");
+	}
+
+	db.collection("blog")
+		.where("title", "==", titleize(req.query.post_id))
+		.get()
+		.then(function(querySnapshot) {
+			if (querySnapshot.empty) {
+				return res.status(404).json({ message: `Post '${req.query.post_id}' was not found :(` });
+			}
+			let responses = [];
+			querySnapshot.forEach(doc => {
+				responses.push(doc.data());
+			});
+			return res.status(200).json(responses);
+		})
+		.catch(error => {
+			// console.log("Error getting documents: ", error);
+			return res.status(500).json({ message: `Internal server error ${error}` });
+		});
+});
