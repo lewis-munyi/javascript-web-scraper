748e759f34d2a5fccf7ff03ca5819387d156da79
lewismunyi <lewismunyi97@gmail.com>
2020-01-08 11:41:44 +0300

    Firebase admin

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require("firebase-functions");
 let serviceAccount = require("./firebase-adminsdk.json");
-
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require("firebase-admin");
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount)
 });
