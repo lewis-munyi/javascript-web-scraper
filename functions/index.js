1ae0a1de366c2c4aae9c26fc9617b0314e250222
lewismunyi <lewismunyi97@gmail.com>
2020-01-08 11:44:24 +0300

    Imported axios, cheerio and cors

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,15 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require("firebase-functions");
-let serviceAccount = require("./firebase-adminsdk.json");
+const axios = require("axios"); // Make Http Requests
+const cheerio = require("cheerio"); // Parse HTML pages
+const cors = require("cors")({ origin: true }); // Make cross-origin requests
+const functions = require("firebase-functions"); // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const serviceAccount = require("./firebase-adminsdk.json"); // Auth credentials
+
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require("firebase-admin");
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount)
 });
+// Initialize firestore
+let db = admin.firestore();
+let FieldValue = admin.firestore.FieldValue;
+let FieldPath = admin.firestore.FieldPath;
